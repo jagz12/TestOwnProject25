012053e282db359fb36701cd07dc2c329ea61786
Jag <jagz12@hotmail.co.uk>
2025-06-21 20:20:24 +0100

    Operator Lab

---
+++ Operators_Lab_Starter/src/main/java/com/sparta/Exercises.java
@@ -0,0 +1,42 @@
+package com.sparta;
+
+public class Exercises {
+
+    // what does this method do?  Add unit tests for this behaviour
+    public static boolean myMethod(int num1, int num2)
+    {
+        if (num1 == num2) return false;
+        return (num1 % num2) == 0;
+    }
+
+    // implement this method so that it returns true if num is even, otherwise false
+    public static boolean evenOdd(int num) {
+        throw new RuntimeException();
+    }
+	
+	 // correct this method - it should multiply x by y and return the result plus one
+    public static int anotherMethod(int x, int y) {
+        x *= x * y;
+        return x++;
+    }
+	
+	    // correct this method so that it passes its tests
+    public static String stringMethod(String theString) {
+        if (theString != null & theString.toUpperCase().startsWith("H")) {
+            return "Starts with H";
+        }
+        return "Doesn't start with H";
+    }
+	
+	// implement this method so that it returns
+    // the sum of all numbers between 1 and n inclusive
+    // that are divisible by either 2 or 5
+    public static int sumEvenFive(int max) {
+        return -1;
+    }
+	
+
+	
+
+
+}
